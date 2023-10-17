42b3239f19de465ce2604ff1fb7290a59cecb42e
Adrian Bumbu <adibumbu91@gmail.com>
2023-10-17 10:51:10 +0300

    Update Source.cpp

--- Measure Time Complexity/Source.cpp
+++ Measure Time Complexity/Source.cpp
@@ -7,8 +7,8 @@ int main()
     // insert the algorithm or a fuction
     auto stop = std::chrono::high_resolution_clock::now(); // Stop measuring time
 
+    // Calculate the duration in microseconds
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
-// Calculate the duration in microseconds
 
     std::cout << "\nTime taken by algorithm: " << duration.count() << " microseconds" << std::endl;
 }
